1ad096c0ba773442966f70004e3fe777d0e78030
Princeantwi2k <antwi2k@gmail.com>
2022-11-09 11:57:02 +0000

    update

--- lib/SellerApp/terms_page.dart
+++ lib/SellerApp/terms_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:new_app/SellerApp/seller_dashboad_page.dart';
 
 class TermsAndConditionPage extends StatelessWidget {
   const TermsAndConditionPage({super.key});
@@ -39,11 +40,51 @@ class TermsAndConditionPage extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.all(18.0),
                 child: Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation  ullamco laboris nisi ut aliquip ex ea .",
                     style: GoogleFonts.roboto(fontSize: 17)),
               ),
               SizedBox(
                 height: 10,
+              ),
+              Row(
+                children: [
+                  Checkbox(value: true, onChanged: (bool? value) {}),
+                  Text("I accept all Terms & conditions")
+                ],
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Row(
+                children: [
+                  Checkbox(value: true, onChanged: (bool? value) {}),
+                  Text("I accept user fee")
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) => SellerDashboad()));
+                  },
+                  child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 148, 203, 219),
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Center(
+                          child: Text(
+                        "Complete",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ))),
+                ),
               ),
             ],
           ),
